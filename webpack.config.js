8d79d616d2e6393260998f267275e459039591e3
Hasan Siddique <hasansiddique@wanclouds.net>
2017-11-24 23:58:50 +0500

    added separate vendor entry point

--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,58 @@ const path = require('path');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    context: path.resolve('client'),
-    devtool: 'source-map',
-    entry: [
-        'eventsource-polyfill', // necessary for hot reloading with IE
-        'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-        path.resolve(__dirname, 'client/app')
-    ],
-    target: 'web',
-    output: {
-        path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'client')
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        //new BundleAnalyzerPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                exclude: /node_modules/,
-                include: path.join(__dirname, 'client'),
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                ['es2015', {modules: false}],
-                                'react',
-                            ],
-                        }
-                    }
-                ]
-            },
-            {
-                test: /(\.css)$/, use: [{loader: 'style-loader'}, {
-                loader: 'css-loader', options: {
-                    module: true
-                }
-            }]
-            },
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-            {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+  context: path.resolve('client'),
+  devtool: 'source-map',
+  entry: {
+    app: ['eventsource-polyfill', // necessary for hot reloading with IE
+      'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+      path.resolve(__dirname, 'client/app')],
+    vendor: ['react', 'react-dom', 'react-redux', 'lodash', 'react-router-dom', 'redux-thunk', 'jquery', 'redux', 'rxjs'],
+  },
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'client')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', minChunks: Infinity})
+    //new BundleAnalyzerPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'client'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['es2015', {modules: false}],
+                'react',
+              ],
+            }
+          }
         ]
-    }
+      },
+      {
+        test: /(\.css)$/, use: [{loader: 'style-loader'}, {
+        loader: 'css-loader', options: {
+          module: true
+        }
+      }]
+      },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
 };
